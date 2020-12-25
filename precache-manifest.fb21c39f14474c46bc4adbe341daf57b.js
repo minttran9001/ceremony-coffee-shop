35ae942b1551367d4c21e02766594bdffece9509
Mint Project <minttran.9002@gmail.com>
2020-12-25 19:27:22 +0700

    Updates

---
+++ precache-manifest.fb21c39f14474c46bc4adbe341daf57b.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177427a9ada303dbbdc0d5e400ff9670",
+    "url": "/ceremony-coffee-shop/index.html"
+  },
+  {
+    "revision": "b1ded2a698ae7d971741",
+    "url": "/ceremony-coffee-shop/static/css/main.e9905d80.chunk.css"
+  },
+  {
+    "revision": "f1661c50215d3333279e",
+    "url": "/ceremony-coffee-shop/static/js/2.815262f7.chunk.js"
+  },
+  {
+    "revision": "fd6c6632fbb7273ee40509e0e1263fe3",
+    "url": "/ceremony-coffee-shop/static/js/2.815262f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1ded2a698ae7d971741",
+    "url": "/ceremony-coffee-shop/static/js/main.68035cef.chunk.js"
+  },
+  {
+    "revision": "f9c2ddf1f8ba311c6ba1",
+    "url": "/ceremony-coffee-shop/static/js/runtime-main.9e7ce469.js"
+  },
+  {
+    "revision": "6a114e21f80c47708a61bf0c99619841",
+    "url": "/ceremony-coffee-shop/static/media/120004199_317188836049534_8314362439864175463_n.6a114e21.jpg"
+  },
+  {
+    "revision": "c6bd4efa5836c724bbf9d229927cab63",
+    "url": "/ceremony-coffee-shop/static/media/122080107_731682854360910_7992613370772055655_n.c6bd4efa.jpg"
+  },
+  {
+    "revision": "231b17fd9605aa3e0490302b59bd2295",
+    "url": "/ceremony-coffee-shop/static/media/123756455_2700438156888383_2212964200339847941_n.231b17fd.jpg"
+  },
+  {
+    "revision": "920f6328f54237225f49628f04ee44d0",
+    "url": "/ceremony-coffee-shop/static/media/69578051_2385501495106508_5488017913804568493_n.920f6328.jpg"
+  },
+  {
+    "revision": "09aed83fd0492b257ec3a23528c867ad",
+    "url": "/ceremony-coffee-shop/static/media/92945641_550843799150043_4835329670101039652_n.09aed83f.jpg"
+  },
+  {
+    "revision": "71d3af0bafb06c80654a0c73e2025374",
+    "url": "/ceremony-coffee-shop/static/media/Bonavita_Immersion_Dripper.71d3af0b.png"
+  },
+  {
+    "revision": "a131f7ab9002d2c8ae1388ba08224622",
+    "url": "/ceremony-coffee-shop/static/media/Capresso_BurrGrinder.a131f7ab.png"
+  },
+  {
+    "revision": "368b94e86626e8f2617368ec351023ac",
+    "url": "/ceremony-coffee-shop/static/media/FieldTrip-Maize-Hero.368b94e8.png"
+  },
+  {
+    "revision": "36bad0176b0e402108be158cbeff2222",
+    "url": "/ceremony-coffee-shop/static/media/FieldTrip-Rust-hero.36bad017.png"
+  },
+  {
+    "revision": "83c685612a2e4858bd2fee3d19723136",
+    "url": "/ceremony-coffee-shop/static/media/Subscriptions-Box_640x.83c68561.png"
+  },
+  {
+    "revision": "bf016faa92d1ddf0efd25614fde992e4",
+    "url": "/ceremony-coffee-shop/static/media/_DSC0377.bf016faa.jpg"
+  },
+  {
+    "revision": "79e5df9eb1468269f04209f913a9bd2c",
+    "url": "/ceremony-coffee-shop/static/media/airpay-la-gi-2.79e5df9e.png"
+  },
+  {
+    "revision": "032075ceb8a8769cd6befa7e9cc07296",
+    "url": "/ceremony-coffee-shop/static/media/apple-pink.032075ce.jpg"
+  },
+  {
+    "revision": "44b6fab9375341c0f4c16e10f11d1322",
+    "url": "/ceremony-coffee-shop/static/media/beans2.44b6fab9.png"
+  },
+  {
+    "revision": "f037215288424f1b303be99ff5e1d4f7",
+    "url": "/ceremony-coffee-shop/static/media/beans3.f0372152.png"
+  },
+  {
+    "revision": "c6b762915f76f7afb9f4db4f4359f334",
+    "url": "/ceremony-coffee-shop/static/media/beans5.c6b76291.png"
+  },
+  {
+    "revision": "a6e466a9cc4b3d8e7e3ee511eacdcbe7",
+    "url": "/ceremony-coffee-shop/static/media/beans6.a6e466a9.png"
+  },
+  {
+    "revision": "3c2e0df79d508ce346a48321f3d48d01",
+    "url": "/ceremony-coffee-shop/static/media/beth_ext1-edit-900x900.3c2e0df7.jpg"
+  },
+  {
+    "revision": "36a020e57a07980da54aec3600bea2d9",
+    "url": "/ceremony-coffee-shop/static/media/ceremony-coffee-roasters-baltimore-cafe-hero2-900x900.36a020e5.jpg"
+  },
+  {
+    "revision": "5cac330c66da978cb31c50d6c1eefa1c",
+    "url": "/ceremony-coffee-shop/static/media/ceremony-coffee-roasters-gear-promo.5cac330c.jpg"
+  },
+  {
+    "revision": "9d26637ed81d5bc0a38d93c2d5a36701",
+    "url": "/ceremony-coffee-shop/static/media/ceremony-coffee-roasters-gear-promo2.9d26637e.jpg"
+  },
+  {
+    "revision": "83fe00fe8a733659eb05216dd4008272",
+    "url": "/ceremony-coffee-shop/static/media/ceremony-coffee-roasters-harbor_point-cafe-hero3-900x900.83fe00fe.jpg"
+  },
+  {
+    "revision": "9e3e0fe73a5d03575b481c134d699f40",
+    "url": "/ceremony-coffee-shop/static/media/ceremony-coffee-roasters-mt.vernon-cafe-hero2-1-900x900.9e3e0fe7.jpg"
+  },
+  {
+    "revision": "9a7118a8a4e9f53a56c8316b0a935ffd",
+    "url": "/ceremony-coffee-shop/static/media/ceremony-coffee-roasters-riva-cafe-hero4-900x900.9a7118a8.jpg"
+  },
+  {
+    "revision": "743c1fafdd9ca58b98d03d8e9f2708e9",
+    "url": "/ceremony-coffee-shop/static/media/ceremony-coffee-roasters-roastery-cafe-hero2-900x900.743c1faf.jpg"
+  },
+  {
+    "revision": "4885c8ed414df5a2062955634c57d15e",
+    "url": "/ceremony-coffee-shop/static/media/ceremony-coffee-roasters-wholesale-detail2-900x900.4885c8ed.jpg"
+  },
+  {
+    "revision": "b181297dc63924f01cdd036e5f0a309b",
+    "url": "/ceremony-coffee-shop/static/media/ceremony-coffee-roasters-wholesale-detail3-900x900.b181297d.jpg"
+  },
+  {
+    "revision": "06a9b6f749f8e1565059c062d0e5f8a0",
+    "url": "/ceremony-coffee-shop/static/media/ceremony-coffee-roasters-wholesale-ordering-and-support.06a9b6f7.jpg"
+  },
+  {
+    "revision": "5dbc5abae1688201be5a62ae032322a7",
+    "url": "/ceremony-coffee-shop/static/media/ceremony-coffee-roasters-wholesale-other-stuff.5dbc5aba.jpg"
+  },
+  {
+    "revision": "ba890e752c2a1a27af2c68f7216fa1d9",
+    "url": "/ceremony-coffee-shop/static/media/ceremony-hero-1.ba890e75.jpg"
+  },
+  {
+    "revision": "0fd7b68ac34604e4c30a6c6dbb415b15",
+    "url": "/ceremony-coffee-shop/static/media/ceremony-order-app-hero-1.0fd7b68a.jpg"
+  },
+  {
+    "revision": "d2583a105412316158b4ddd8af59de7e",
+    "url": "/ceremony-coffee-shop/static/media/climateprojecthero-1.d2583a10.png"
+  },
+  {
+    "revision": "ed7572e37a7364056f0e97fec42cb740",
+    "url": "/ceremony-coffee-shop/static/media/close.ed7572e3.svg"
+  },
+  {
+    "revision": "cf2f8fc04a3b379d0b2a943d9e19c91e",
+    "url": "/ceremony-coffee-shop/static/media/cmy-coldbrew-hero-2-1600x900.cf2f8fc0.jpg"
+  },
+  {
+    "revision": "3a8a65e65b3f92bb07a93dfebf9af352",
+    "url": "/ceremony-coffee-shop/static/media/cmy-coldbrew-pouring-1-800x999.3a8a65e6.jpg"
+  },
+  {
+    "revision": "30d2fe04571e99e0523547e68934be05",
+    "url": "/ceremony-coffee-shop/static/media/coldbrew-contact.30d2fe04.jpg"
+  },
+  {
+    "revision": "deb08535e0ec713f6050a95b5959f15b",
+    "url": "/ceremony-coffee-shop/static/media/coldbrew-massive.deb08535.jpg"
+  },
+  {
+    "revision": "b8c2e344983731f605c644d5d73727ed",
+    "url": "/ceremony-coffee-shop/static/media/coldbrew-service.b8c2e344.mp4"
+  },
+  {
+    "revision": "674ca3999bbb5ddf8ebe26a68a3ba2a1",
+    "url": "/ceremony-coffee-shop/static/media/coldbrew.674ca399.jpg"
+  },
+  {
+    "revision": "fc5228d2ede3dfa3e8c2d4110953d475",
+    "url": "/ceremony-coffee-shop/static/media/coloboxset-slider3.fc5228d2.jpg"
+  },
+  {
+    "revision": "3061467dc4a9d8ffb9a08be013c937f8",
+    "url": "/ceremony-coffee-shop/static/media/cross-st-header1-900x900.3061467d.jpg"
+  },
+  {
+    "revision": "84b68d226d16f1b1baf040168058688b",
+    "url": "/ceremony-coffee-shop/static/media/details.84b68d22.jpg"
+  },
+  {
+    "revision": "c5fc1771f78b332d8d31efb7b2a9352a",
+    "url": "/ceremony-coffee-shop/static/media/ecommslider3.c5fc1771.jpg"
+  },
+  {
+    "revision": "898a2978f38f6c1193d83c1ebfa8a7ff",
+    "url": "/ceremony-coffee-shop/static/media/facebook.898a2978.svg"
+  },
+  {
+    "revision": "a70e5c5a393ad41aff3a28d9e51fc753",
+    "url": "/ceremony-coffee-shop/static/media/fieldtriphat-green.a70e5c5a.jpg"
+  },
+  {
+    "revision": "d69f1629cc5d278b37a4ba2f58f1714b",
+    "url": "/ceremony-coffee-shop/static/media/gift.d69f1629.d69f1629.svg"
+  },
+  {
+    "revision": "505eab38ee30b2cfe91b97b76e65fc47",
+    "url": "/ceremony-coffee-shop/static/media/holiday20hero.505eab38.jpg"
+  },
+  {
+    "revision": "a07e24b6bfc6f9a6af2f687370e33652",
+    "url": "/ceremony-coffee-shop/static/media/home-4-image-2.a07e24b6.png"
+  },
+  {
+    "revision": "9278185feeebcf2a56fb0b5e130c9589",
+    "url": "/ceremony-coffee-shop/static/media/home-4-image-5.9278185f.png"
+  },
+  {
+    "revision": "c72c4f37fd84160a595575966b7e5771",
+    "url": "/ceremony-coffee-shop/static/media/home_wanchai_shorter.c72c4f37.mp4"
+  },
+  {
+    "revision": "bc83123bcf5cc587cf60093140e206b5",
+    "url": "/ceremony-coffee-shop/static/media/instagram.bc83123b.svg"
+  },
+  {
+    "revision": "6b1872c3c2e06a0d2fea7d07362ec831",
+    "url": "/ceremony-coffee-shop/static/media/itunes.6b1872c3.svg"
+  },
+  {
+    "revision": "75f34b4fdfd49f861d920c313994bf58",
+    "url": "/ceremony-coffee-shop/static/media/landing-2-img-3.75f34b4f.png"
+  },
+  {
+    "revision": "63096d9f96c9c7a7d244e71a33d019ab",
+    "url": "/ceremony-coffee-shop/static/media/landing-2-img-7.63096d9f.png"
+  },
+  {
+    "revision": "751abc7787b4b56c2a0926d2d28c73b6",
+    "url": "/ceremony-coffee-shop/static/media/logo-black.751abc77.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ceremony-coffee-shop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "be237328d9c252b1b9133a02c3be3e5e",
+    "url": "/ceremony-coffee-shop/static/media/next.be237328.svg"
+  },
+  {
+    "revision": "f40f2787a0fb7abf99122196d2c6b789",
+    "url": "/ceremony-coffee-shop/static/media/nitrohandle-ceremony-coffee-recrop-800x999.f40f2787.jpg"
+  },
+  {
+    "revision": "16ede9ee2e270cfcfb3c82c88a191861",
+    "url": "/ceremony-coffee-shop/static/media/pre.16ede9ee.svg"
+  },
+  {
+    "revision": "ac49b5122d4d22cfd34e848144fc14d2",
+    "url": "/ceremony-coffee-shop/static/media/redbluetumblers-hero.ac49b512.jpg"
+  },
+  {
+    "revision": "540eb0e8114a38d9a12d2665a27a823b",
+    "url": "/ceremony-coffee-shop/static/media/serve.540eb0e8.mp4"
+  },
+  {
+    "revision": "ad86ad34f8f0ecddfe10f2e622985f3e",
+    "url": "/ceremony-coffee-shop/static/media/sub.ad86ad34.jpg"
+  },
+  {
+    "revision": "081c797881900a99473d0d5cf3981b63",
+    "url": "/ceremony-coffee-shop/static/media/subbanner.081c7978.jpg"
+  },
+  {
+    "revision": "2ef93fc3237b25a8fc1139e3b0e10fe9",
+    "url": "/ceremony-coffee-shop/static/media/tagline.2ef93fc3.svg"
+  },
+  {
+    "revision": "b14fa7436ecaca4a611f33c35b5ef49d",
+    "url": "/ceremony-coffee-shop/static/media/teamcupping.b14fa743.jpg"
+  },
+  {
+    "revision": "23f3e52571dca7698c69b0ed96f3e3b7",
+    "url": "/ceremony-coffee-shop/static/media/twitter.23f3e525.svg"
+  },
+  {
+    "revision": "0a59ee7032a23a4ad1e67ad05b83bb5b",
+    "url": "/ceremony-coffee-shop/static/media/up-arrow.0a59ee70.svg"
+  },
+  {
+    "revision": "61e48df7211e1b96ad0cf4c61e21f8ff",
+    "url": "/ceremony-coffee-shop/static/media/whall-1-1596x1080.61e48df7.jpg"
+  },
+  {
+    "revision": "bde45958e84c591be9bb125416e17bcf",
+    "url": "/ceremony-coffee-shop/static/media/whoppers-1.bde45958.jpg"
+  }
+]);
